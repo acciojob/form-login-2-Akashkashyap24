25acef0ab513b225f1b3a311afb6aaab113a8990
Akashkashyap24 <144315919+Akashkashyap24@users.noreply.github.com>
2024-01-25 13:35:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let firstname=document.getElementById("fn");
+let lastname=document.getElementById("ln");
+let phonenum=document.getElementById("pn");
+let emailid=document.getElementById("e");
+let sumbit=document.getElementById("b");
+sumbit..addEventListener('click'()=>{
+	alert("i am work");
+})
+
